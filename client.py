b833f7ef47aabbb7b2bc57c3f88f94267762c099
Neutralix <58196205+neutralix@users.noreply.github.com>
2021-06-08 21:43:26 +0700

    Add fitur kirim file
    Program server dan klien dapat menangani perintah kirim file ke teman.  Klien akan mengirimkan file ke server lalu diteruskan ke klien penerima.

--- client.py
+++ client.py
@@ -1,15 +1,39 @@
 import socket
 import sys
 import threading
+import os
 
 def read_msg(sock_cli):
     while True:
         data = sock_cli.recv(65535)
+
+        datasplit = data.split(b'|', 1)
+        header = datasplit[0]
+        msg = datasplit[1]
+        # print(header)
+        # print(msg)
         if len(data) == 0:
             break
-            
-        print(data)
+        
+        if header == b"file":
+            msgsplit = msg.split(b'|', 3)
+            msgprint = msgsplit[0].decode('utf-8')
+            file_name = msgsplit[1].decode('utf-8')
+            file_size  = int(msgsplit[2].decode('utf-8'))
+            file_data = msgsplit[3]
 
+            with open(file_name, "wb") as f:
+                f.write(file_data)
+                while (file_size - len(file_data) > 0):
+                    data = sock_cli.recv(65535)
+                    file_data += data
+                    f.write(data)
+                
+            
+            print("menerima file {}".format(file_name))
+        elif header == b"txt":
+            print("\n" + msg.decode('utf-8'))
+            
 # check input username
 if len(sys.argv) != 2:
     sys.exit()
@@ -23,31 +47,42 @@ thread_cli.start()
 
 while True:
     # input command
-    cmd = input("masukan perintah(bcast untuk broadcast, add untuk add friend, send_file untuk send file, send)")
+    cmd = input("<command list>\nbcast -> broadcast pesan ke semua teman\nmsg -> kirim pesan private ke teman\nadd -> request untuk jadi teman\nacc -> terima permintaan teman\nfile -> kirim file ke teman\nexit -> tutup program\n>>> ")
 
     if cmd == "bcast":
         dest = "bcast"
-        msg = input("masukan pesan :")
+        msg = input("masukan pesan : ")
     elif cmd == "msg":
-        dest = input("masukan username tujuan :")
-        msg = input("masukan pesan:")
+        dest = input("masukan username tujuan : ")
+        msg = input("masukan pesan: ")
     elif cmd == "add":
-        dest = input("masukan username untuk request friend :")
+        dest = input("masukan username untuk request friend : ")
         msg = "request friend"
     elif cmd == "acc":
-        dest = input("masukan username untuk accept friend :")
+        dest = input("masukan username untuk accept friend : ")
         msg = "accept friend"
     elif cmd == "file":
-        dest = input("masukan username tujuan :")
-        nama_file = input("masukan nama file :")
-        msg = "data file"
+        dest = input("masukan username tujuan : ")
+        file_name = input("masukan nama file : ")
+        msg = "mengirim file {}".format(file_name)
+    elif cmd == "exit":
+        sock_cli.close()
+        break
     else:
         print("command not found")
         continue
-  
 
-    if msg == "exit":
-        sock_cli.close()
-        break
+    if cmd == "file":
+        if not os.path.exists(file_name):
+            print('File tidak ditemukan.')
+            continue
 
-    sock_cli.send(bytes("{}|{}|{}".format(cmd, dest, msg), "utf-8"))
+        file_data = b''
+        file_size = 0
+        with open(file_name, "rb") as f:
+            file_data = f.read()
+            file_size += len(file_data)
+
+        sock_cli.sendall(bytes("{}|{}|{}|{}|{}|".format(cmd, dest, msg, file_name, file_size), "utf-8") + file_data)
+    else: 
+        sock_cli.send(bytes("{}|{}|{}".format(cmd, dest, msg), "utf-8"))
--- server.py
+++ server.py
@@ -6,24 +6,46 @@ import threading
 def read_cmd(clients, friend_list, sock_cli, addr_cli, username_cli):
     while True:
         data = sock_cli.recv(65535)
+
+        print(data)
+        # print("\n\n\n LOOOP BARUUUU \n\n\n")
+
         if len(data) == 0:
             break
         
-        cmd, dest, msg = data.decode("utf-8").split("|")
-        msg = "<{}>: {}".format(username_cli, msg)
+        cmd, dest, msg = data.split(b"|", 2)
+        cmd = cmd.decode('utf-8')
+        dest = dest.decode('utf-8')
 
         if cmd == "bcast":
+            msg = msg.decode('utf-8')
+            msg = "<{}>: {}".format(username_cli, msg)
             send_broadcast(clients, friend_list, addr_cli, username_cli, msg)
         elif cmd == "msg":
+            msg = msg.decode('utf-8')
+            msg = "<{}>: {}".format(username_cli, msg)
             send_msg(clients, friend_list, dest, sock_cli, username_cli, msg)
         elif cmd == "add":
+            msg = msg.decode('utf-8')
+            msg = "<{}>: {}".format(username_cli, msg)
             add_friend(clients, friend_list, dest, sock_cli, username_cli)
         elif cmd == "acc":
+            msg = msg.decode('utf-8')
+            msg = "<{}>: {}".format(username_cli, msg)
             accept_friend(clients, friend_list, dest, sock_cli, username_cli)
         elif cmd == "file":
-            send_file(clients, friend_list, dest, addr_cli, msg)
-        print(data)
+            filemsg = msg.split(b"|", 3)
+            msg = filemsg[0].decode('utf-8')
+            file_name = filemsg[1].decode('utf-8')
+            file_size = int(filemsg[2].decode('utf-8'))
+            file_data = filemsg[3]
     
+            while (file_size - len(file_data) > 0):
+                data = sock_cli.recv(65535)
+                file_data += data
+
+            send_file(clients, friend_list, dest, sock_cli, username_cli, msg, file_name, file_size, file_data)
+        
     sock_cli.close()
     print("Connection closed", addr_cli)
 
@@ -31,36 +53,37 @@ def send_broadcast(clients, friend_list, sender_addr_cli, username_cli, msg):
    for key in list(clients.keys()):
         if not (sender_addr_cli[0] == clients[key][1][0] and sender_addr_cli[1] == clients[key][1][1]):
             if (key, username_cli) in friend_list and (username_cli, key) in friend_list:
-                clients[key][0].send(bytes(msg, "utf-8"))
+                bcastmsg = "txt|{}".format(msg)
+                clients[key][0].send(bytes(bcastmsg, "utf-8"))
 
 def send_msg(clients, friend_list, dest, sender_sock, username_cli, msg):
     #check friend
     if (dest, username_cli) in friend_list and (username_cli, dest) in friend_list:
         sock_cli = clients[dest][0]
+        msg = "txt|{}".format(msg)
         sock_cli.send(bytes(msg, "utf-8"))
     else:
-        error_msg = "Not friend with {}".format(dest)
+        error_msg = "txt|Not friend with {}".format(dest)
         sender_sock.send(bytes(error_msg, "utf-8"))
     
-
 def add_friend(clients, friend_list, dest, sender_sock, username_cli):
     if (username_cli, dest) in friend_list and (dest, username_cli) in friend_list:
-        msg = "already friend with {}".format(dest)
+        msg = "txt|already friend with {}".format(dest)
         sender_sock.send(bytes(msg, "utf-8"))
     elif (username_cli, dest) in friend_list and (dest, username_cli) not in friend_list:
-        msg = "already request friend to {}, but still not accepted".format(dest)
+        msg = "txt|already request friend to {}, but still not accepted".format(dest)
         sender_sock.send(bytes(msg, "utf-8"))
     elif (dest, username_cli) in friend_list and (username_cli, dest) not in friend_list:
-        msg = "{} already request to be friend with you, use accept_friend command to accept".format(dest)
+        msg = "txt|{} already request to be friend with you, use accept_friend command to accept".format(dest)
         sender_sock.send(bytes(msg, "utf-8"))
     else:
         friend_list.add((username_cli, dest))
         # send to sender
-        msg = "request sent"
+        msg = "txt|request sent"
         sender_sock.send(bytes(msg, "utf-8"))
         # send to receiver
         sock_cli = clients[dest][0]
-        msg = "{} send you a friend request".format(username_cli)
+        msg = "txt|{} send you a friend request".format(username_cli)
         sock_cli.send(bytes(msg, "utf-8"))
     
     #check friend list
@@ -68,24 +91,32 @@ def add_friend(clients, friend_list, dest, sender_sock, username_cli):
 
 def accept_friend(clients, friend_list, dest, sender_sock, username_cli):
     if (username_cli, dest) in friend_list and (dest, username_cli) in friend_list:
-        msg = "already friend with {}".format(dest)
+        msg = "txt|already friend with {}".format(dest)
         sender_sock.send(bytes(msg, "utf-8"))
     elif (dest, username_cli) in friend_list and (username_cli, dest) not in friend_list:
         friend_list.add((username_cli, dest))
         # send to sender
-        msg = "you are now friend with {}".format(dest)
+        msg = "txt|you are now friend with {}".format(dest)
         sender_sock.send(bytes(msg, "utf-8"))
         # send to receiver
         sock_cli = clients[dest][0]
-        msg = "{} accepted your friend request".format(username_cli)
+        msg = "txt|{} accepted your friend request".format(username_cli)
         sock_cli.send(bytes(msg, "utf-8"))
     else:
-        msg = "you have no friend request from {}".format(dest)
+        msg = "txt|you have no friend request from {}".format(dest)
         sender_sock.send(bytes(msg, "utf-8"))
     
     # check friend list
     print(friend_list)
 
+def send_file(clients, friend_list, dest, sender_sock, username_cli, msg, file_name, file_size, file_data):
+    #check friend
+    if (dest, username_cli) in friend_list and (username_cli, dest) in friend_list:
+        sock_cli = clients[dest][0]
+        sock_cli.sendall(bytes("file|{}|{}|{}|".format(msg, file_name, file_size), "utf-8") + file_data)
+    else:
+        error_msg = "txt|Not friend with {}".format(dest)
+        sender_sock.send(bytes(error_msg, "utf-8"))
 
 sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_server.bind(("0.0.0.0", 6666))
